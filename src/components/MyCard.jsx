8a09fe65e75af6410f36f97522014bd157b73f8a
Cristian Venegas <cristian.venegas@materoceano.com>
2022-10-11 18:05:37 -0300

    fix : card style

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -3,25 +3,31 @@ import "../assets/css/card.css";
 
 const MyCard = ({name, img, price,ingredients}) => {
   return (
-    <div className="card">
-      <div className="card-banner">
-        <p className="category-tag popular">Pizza</p>
-        <img className="banner-img" src={img} alt="" />
-      </div>
-      <div className="card-body">
-        <p className="blog-hashtag">#Food #Pizza</p>
-        <h2 className="blog-title">{name}</h2>
-        <p className="blog-description"></p>
-        <div className="card-profile">
-          <div className="card-profile-info">
-            <h3 className="profile-name">{price}</h3>
-            {ingredients.map((item, index) => <p key={index}>{item}</p>)}
-            <p className="profile-followers">5.2K</p>
-          </div>
-        </div>
-      </div>
+   <>
+   <div className="card">
+    <div className="img">
+      <img src={img} alt="pizza" />
     </div>
+    <div className="name">
+      <h2>{name}</h2>
+    </div>
+    <div className="ingredientes">
+      
+        {ingredients.map((item, index) => <p key={index}>{item}</p> )}
+      
+    </div>
+    <div className="price">
+      <h2>{price}</h2>
+    </div>
+    <div className="buttons">
+      <button>Ver Mas</button>
+      <button>Añadir</button>
+    </div>
+   </div>
+   </>
   );
 };
 
 export default MyCard;
+
+
