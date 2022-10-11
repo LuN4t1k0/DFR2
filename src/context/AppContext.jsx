f08e08301bd983e5a40b954f3e89b934cec1c398
Cristian Venegas <cristian.venegas@materoceano.com>
2022-10-10 21:21:17 -0300

    add : add pizza and ingredientes States

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,7 +1,9 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 export const AppContext = createContext()
 
 const AppProvider = ({children}) => {
+  const [carro, setCarro] = useState([])
+  const [ingrediente, setIngrediente] = useState([])
   return (
     <AppContext.Provider value={{}}>
       {children}
