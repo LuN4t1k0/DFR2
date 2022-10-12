b5c1d2ed32b501e5f9e9addb2e0509b30eca0743
Cristian Venegas <cristian.venegas@materoceano.com>
2022-10-12 13:13:36 -0300

    add : navbar

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const MyNavBar = () => {
   return (
-<nav className='flex justify-between text-white bg-black w-full h-20 items-center p-5'>
-  <div className="izquierda">Pizzeria Mamma Mia!</div>
-  <div className="flex justify-around sm:w20 w-60 centro">
-    <div><NavLink>Home</NavLink></div>
-    <div><NavLink>Home</NavLink></div>
-  </div>
-  <div className="derecha"><i class="fa-solid fa-cart-shopping"></i></div>
-</nav>
-  )
-}
+    <nav className="flex justify-between text-white bg-black w-full h-20 items-center p-5">
+      <div className="izquierda">Pizzeria Mamma Mia!</div>
+      <div className="flex justify-around sm:w20 w-60 centro">
+        <div>
+          <NavLink>Home</NavLink>
+        </div>
+        <div>
+          <NavLink>Home</NavLink>
+        </div>
+      </div>
+      <div className="derecha">
+        <div className="cantidad">2</div>
+        <div className="carro">
+          <i className=" fa-solid fa-cart-shopping"></i>
+        </div>
+        <div className="total">
+          <h3>Total : $18.500 </h3>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default MyNavBar
+export default MyNavBar;
