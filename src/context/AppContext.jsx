2240dffd99d9ec0037af3fe7f727a20f181c7a6b
Cristian Venegas <cristian.venegas@materoceano.com>
2022-10-12 11:52:55 -0300

    feat :remove and add products

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,24 +3,54 @@ import useFetch from "../hooks/useFetch";
 export const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
   const data = useFetch("/pizzas.json");
 
   //vaciar carro de compras
-  const cleanCart =() => {
-    setCart([])
-  }
+  const cleanCart = () => {
+    setCart([]);
+  };
 
   //existe producto
   const isInCart = (id) => {
-    cart.find(item => item.id === id ) ? true : false
-  }
+    cart.find((item) => item.id === id) ? true : false;
+  };
 
-  
+  //remover producto
+  const removeProduct = (id) => {
+    setCart(cart.filter((item) => item.id != id));
+  };
 
-  return (<AppContext.Provider value={{ data }}>
-    {children}
-    </AppContext.Provider>);
+  //agregar Producto
+  const addProduct = (product, quantity) => {
+    if (isInCart(product.id)) {
+      setCart(
+        cart.map((item) => {
+          return item.id === product.id
+            ? { ...item, quantity: item.quantity + quantity }
+            : item;
+        })
+      );
+    } else {
+      setCart([...cart, { product, quantity }]);
+    }
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        addProduct,
+        removeProduct,
+        isInCart,
+        cleanCart,
+        cart,
+        setCart,
+        data,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export default AppProvider;
